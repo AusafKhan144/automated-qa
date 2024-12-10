e8757eeac417fd2a8bdc864ef9b268ecf6ea90c1
Ausaf Khan <kausaf141@gmail.com>
2024-12-10 18:41:58 +0500

    change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="automated_qa",
-    version="0.3.2",
+    version="0.3.3",
     packages=find_packages(),
     install_requires=[
         "requests",  # Add any dependencies your script needs here
